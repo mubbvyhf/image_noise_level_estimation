af2b4cf5aa18fe8d7f30924385e1ef885a5d6f85
mubbvyhf <52276359+mubbvyhf@users.noreply.github.com>
2024-06-19 09:51:46 +0800

    Create NoiseLevelEstimation.h

---
+++ NoiseLevelEstimation.h
@@ -0,0 +1,29 @@
+#include <vector>
+
+#include "opencv2/opencv.hpp"
+
+class NoiseLevelEstimation
+{
+public:
+
+	NoiseLevelEstimation();
+	~NoiseLevelEstimation();
+
+	void estimateNoiseLevel(const cv::Mat& src, float& noiseLevel);
+
+private:
+	void img2patch(int patchSize, int step);
+
+	int d;
+	int step;
+	int patchSize;
+	int numPatches;
+	std::vector<int> mPatchesPtrOffset;
+	std::vector<int> mPatchPixelsOffset;
+	std::vector<int> mPatchesColPixelsOffset;
+	cv::Mat mSrcFloat;
+	cv::Mat mPatches;
+	cv::Mat mX;
+	cv::Mat mSigmaX;
+	cv::Mat mEigenValues;
+};
